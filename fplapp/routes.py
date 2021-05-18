fbaeb85ef1b5c4ea6d84201c939646994d981a5d
Taylor Smith <tjsmith1513@gmail.com>
2021-05-18 13:40:33 -0400

    Added flask and redirect for process-data. Added new route for rank-response

--- fplapp/routes.py
+++ fplapp/routes.py
@@ -12,8 +12,30 @@ def selectteam():
     if request.method == 'POST':
         
         
-        request_league_ID = request.form["leagueID"] 
+        request_user_ID = request.form["userID"] 
         
-        members =  fpl.get_league_teams(request_league_ID)
-       
-        return render_template('selectteam.html', leagueMembers = members) 
+        league_info =  fpl.get_user_leagues_info(request_user_ID)
+        if league_info == False:
+            flash('Oh no something went wrong. Please try again!', 'danger')
+            return redirect(url_for('home'))
+        else:
+            return render_template('selectteam.html', league_info = league_info) 
+
+@app.route('/check-data', methods=['GET', 'POST'])
+def process_data():
+    if request.method == 'POST':
+        
+        
+        rank = int(request.form['select_league'])
+        response = ''
+        if rank == 1:
+            response = 'YES'
+        elif rank > 1 and rank <= 5:
+            response = 'NO'
+        elif rank > 5:
+            response = 'LOL'
+
+        print(rank)
+        return render_template('amiwinning.html', rank_response = response)
+    
+    
