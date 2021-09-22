f253fbd58020f42160dde0a90f31cd50eeecd27c
Taylor Smith <tjsmith1513@gmail.com>
2021-09-22 10:24:25 -0400

    upadated routes to handle fpl_players

--- fplapp/routes.py
+++ fplapp/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, flash, redirect
 
 from fplapp import app
 from fplapp.fpl_data import FplData
+from fplapp import fpl_players
 import json
 
 
@@ -15,7 +16,7 @@ def home():
         return render_template('home.html', data = json.dumps(data), league_info= data, chip_dict=fpl_data.get_chip_count(), chip_used_list = fpl_data.get_chips_used_list(),
                                 most_points_single_gw = fpl_data.get_most_points_scored_in_a_gw(), max_points_per_gw = fpl_data.get_max_points_per_gw(),
                                 count_gw_leader=fpl_data.count_gw_leader(), min_points_per_gw=fpl_data.get_min_points_per_gw(),
-                                count_gw_lowest=fpl_data.count_gw_lowest())
+                                count_gw_lowest=fpl_data.count_gw_lowest(), fpl_players=fpl_players.get_team_player_list())
     else:
         return render_template('errorpage.html', error_message = data)
 
@@ -34,20 +35,20 @@ def home():
 #             return render_template('selectteam.html', league_info = league_info) 
 
 
-# Currently not being used 
-@app.route('/check-data', methods=['GET', 'POST'])
-def process_data():
-    if request.method == 'POST':
-        rank = int(request.form['select_league'])
-        response = ''
-        if rank == 1:
-            response = 'YES'
-        elif rank > 1 and rank <= 5:
-            response = 'NO'
-        elif rank > 5:
-            response = 'LOL'
+# # Currently not being used 
+# @app.route('/check-data', methods=['GET', 'POST'])
+# def process_data():
+#     if request.method == 'POST':
+#         rank = int(request.form['select_league'])
+#         response = ''
+#         if rank == 1:
+#             response = 'YES'
+#         elif rank > 1 and rank <= 5:
+#             response = 'NO'
+#         elif rank > 5:
+#             response = 'LOL'
 
-        print(rank)
-        return render_template('amiwinning.html', rank_response = response)
+#         print(rank)
+#         return render_template('amiwinning.html', rank_response = response)
     
 
