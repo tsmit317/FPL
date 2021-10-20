eae31d2f3773bea8f40ab3b1c17c43aa461e61f3
Taylor Smith <tjsmith1513@gmail.com>
2021-10-20 13:40:37 -0400

    reformat for readablity

--- fplapp/routes.py
+++ fplapp/routes.py
@@ -13,20 +13,22 @@ def home():
     data = fpl_data.get_league_data()
     
     fpl_players = FplPlayers()
-    fpl_players.set_team_player_list(data[0]['gw'][-1])
+    most_recent_gw = data[0]['gw'][-1]
+    fpl_players.set_team_player_list(most_recent_gw) 
     if type(data[0]) is dict:
         
-        return render_template('home.html', data = json.dumps(data),
-                                league_info= data, 
-                                chip_count_dict=fpl_data.get_chip_count(), 
+        return render_template('home.html', 
+                                data = json.dumps(data),
+                                league_info = data, 
+                                chip_count_dict = fpl_data.get_chip_count(), 
                                 chip_used_list = fpl_data.get_chips_used_list(),
                                 most_points_single_gw = fpl_data.get_most_points_scored_in_a_gw(), 
                                 max_points_per_gw = fpl_data.get_max_points_per_gw(),
-                                min_points_per_gw=fpl_data.get_min_points_per_gw(),
-                                count_gw_leader=fpl_data.count_gw_leader(), 
-                                count_gw_lowest=fpl_data.count_gw_lowest(), 
-                                fpl_players=fpl_players.get_team_player_list(), 
-                                sorted_value=sorted(data, key=lambda k: k['total_value'][-1], reverse=True), 
+                                min_points_per_gw = fpl_data.get_min_points_per_gw(),
+                                count_gw_leader = fpl_data.count_gw_leader(), 
+                                count_gw_lowest = fpl_data.count_gw_lowest(), 
+                                fpl_players = fpl_players.get_team_player_list(), 
+                                sorted_value = sorted(data, key=lambda k: k['total_value'][-1], reverse=True), 
                                 league_chips = fpl_data.get_member_chip_list())
     else:
         return render_template('errorpage.html', error_message = data)
