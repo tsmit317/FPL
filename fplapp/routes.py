d6e8e0d75e866b23b078b0de646bc0b41c42502e
Taylor Smith <tjsmith1513@gmail.com>
2021-10-28 20:26:55 -0400

    Updated routes

--- fplapp/routes.py
+++ fplapp/routes.py
@@ -17,13 +17,18 @@ def home():
     fpl_players.set_team_player_list(most_recent_gw) 
     fpl_players.subtract_tranfer_hits_from_current_points(data)
     
+    current_points = fpl_players.get_current_points()
+    
+    league_data = fpl_data.check_total_points_updated(current_points)
+
+    
         
     
     if type(data[0]) is dict:
         
         return render_template('home.html', 
-                                data = json.dumps(data),
-                                league_info = data, 
+                                data = json.dumps(league_data),
+                                league_info = league_data, 
                                 chip_count_dict = fpl_data.get_chip_count(), 
                                 chip_used_list = fpl_data.get_chips_used_list(),
                                 most_points_single_gw = fpl_data.get_most_points_scored_in_a_gw(), 
@@ -32,10 +37,10 @@ def home():
                                 count_gw_leader = fpl_data.count_gw_leader(), 
                                 count_gw_lowest = fpl_data.count_gw_lowest(), 
                                 fpl_players = fpl_players.get_team_player_list(), 
-                                sorted_value = sorted(data, key=lambda k: k['total_value'][-1], reverse=True), 
+                                sorted_value = sorted(league_data, key=lambda k: k['total_value'][-1], reverse=True), 
                                 league_chips = fpl_data.get_member_chip_list(),
                                 player_league_percent = fpl_players.get_player_picked_league_percent(len(data)),
-                                current_points = fpl_players.get_current_points())
+                                current_points = current_points)
     else:
         return render_template('errorpage.html', error_message = data)
 
